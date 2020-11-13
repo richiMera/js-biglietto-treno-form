5f620f5aa80f6554dca29dfe5b1e191db9490912
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-13 15:38:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// var nome = document.getElementById('nome_cognome').value;
+// console.log(nome);
+
+// var km = document.getElementById('kilometri');
+// console.log(nome);
+//
+// var eta = document.getElementById('kilometri');
+// console.log(eta);
+var pulsanteGenera = document.getElementById("genera");
+pulsanteGenera.addEventListener("click",
+function() {
+  var nome = document.getElementById('nome_cognome').value;
+  console.log(nome);
+
+  var kmUtente = document.getElementById("kilometri").value;
+  console.log(kmUtente);
+
+ var etaUtente = document.getElementById("eta").value;
+ console.log(etaUtente);
+  }
+)
