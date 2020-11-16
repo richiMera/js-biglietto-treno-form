41994c2d753475fd0412dd1e3e76ddbc39412d1a
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-16 01:37:33 +0100

    project loop FINISH

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 
+var pulsanteAcquista = document.getElementById("acquista");
+
+var acquistoAvvenuto =  document.getElementById("thanks_container");
+
+// var leftBox = document.getElementById("left_box_js");
+
+var rightBox = document.getElementById("right_box_js");
+
+var ticket = document.getElementById("biglietto");
+
+var infoPannel = document.getElementById("info_pannel");
+
+var thanksText = document.getElementById("thanks_text");
+
+var pulsanteShow = document.getElementById("click_to_show");
+pulsanteShow.addEventListener("click",
+function() {
+  infoPannel = document.getElementById("info_pannel");
+  infoPannel.classList.remove("hidden");
+
+  pulsanteShow.classList.add("hidden");
+
+
+  ticket.classList.add("hidden");
+}
+);
+
 var pulsanteGenera = document.getElementById("genera");
 pulsanteGenera.addEventListener("click",
  function() {
@@ -46,8 +73,22 @@ pulsanteGenera.addEventListener("click",
       console.log(accuracyNormal);
     }
 
+    var infoPannel = document.getElementById("info_pannel");
+    infoPannel.classList.add("transition")
+    infoPannel.classList.add("hidden")
+
     var ticket = document.getElementById("biglietto");
+    ticket.classList.add("transition_delay");
     ticket.style.opacity = 1;
+
+    // document.getElementById("annulla").classList.remove("hidden");
+    document.getElementById("annulla").classList.add("transition_delay");
+    document.getElementById("annulla").classList.add("visible");
+
+    pulsanteAcquista.classList.remove("hidden")
+    pulsanteAcquista.classList.add("transition_delay");
+
+    pulsanteAnnulla.style.opacity = 1;
 
     var nomePasseggero = document.getElementById("nomePasseggero");
     nomePasseggero.innerHTML = nome;
@@ -70,7 +111,22 @@ pulsanteGenera.addEventListener("click",
 var pulsanteAnnulla = document.getElementById("annulla");
 pulsanteAnnulla.addEventListener("click",
  function() {
-   document.getElementById("biglietto").style.opacity = 0;
+
+   pulsanteAnnulla.className = "transition";
+
+   pulsanteAnnulla.style.opacity = 0;
+
+   pulsanteAcquista.className = "transition";
+   pulsanteAcquista.classList.add("hidden");
+
+
+   var ticket = document.getElementById("biglietto");
+   ticket.className = "transition";
+   ticket.style.opacity = 0;
+
+   infoPannel = document.getElementById("info_pannel");
+   infoPannel.classList.add("transition_delay");
+   infoPannel.classList.remove("hidden");
 
    document.getElementById("nome_cognome").value = "";
    document.getElementById("kilometri").value = "";
@@ -82,14 +138,47 @@ pulsanteAnnulla.addEventListener("click",
    document.getElementById("codice").value = "";
    document.getElementById("costo").value = "";
  }
-)
+);
 
-var pulsanteShow = document.getElementById("click_to_show");
-pulsanteShow.addEventListener("click",
+var pulsanteAcquista = document.getElementById("acquista");
+pulsanteAcquista.addEventListener("click",
   function() {
-    var pannelloInfo = document.getElementById("info_pannel");
-    pannelloInfo.classList.remove("hidden");
-    pannelloInfo.classList.add("visible");
-    pulsanteShow.classList.add("hidden");
+
+    ticket.classList.remove("transition_delay");
+
+    pulsanteAcquista.classList.remove("transition_delay");
+
+    pulsanteAnnulla.classList.remove("transition_delay");
+
+    var acquistoAvvenuto =  document.getElementById("thanks_container");
+    acquistoAvvenuto.classList.remove("hidden");
+    acquistoAvvenuto.classList.add("semi_transition");
+    acquistoAvvenuto.style.zIndex = 2;
+
+    thanksText.classList.remove("hidden");
+    thanksText.classList.add("transition_delay");
   }
-)
+);
+
+var tornaIndietro = document.getElementById("turn_back");
+turn_back.addEventListener("click",
+  function() {
+
+    acquistoAvvenuto.classList.add("hidden");
+    acquistoAvvenuto.style.zIndex = 0;
+
+    pulsanteShow.classList.remove("hidden");
+
+    infoPannel.classList.add("hidden");
+
+    // ticket.classList.remove("transition_delay");
+    ticket.style.opacity = 0;
+
+    pulsanteAcquista.classList.add("hidden");
+    // pulsanteAcquista.classList.remove("transition_delay");
+
+    pulsanteAnnulla.style.opacity = 0;
+    // pulsanteAnnulla.classList.remove("transition_delay");
+
+  }
+);
